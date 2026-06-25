49797d7efce72b0ae54799bfe39f80e6ff634cd2
maria45paula <paulaugggvbk@gmail.com>
2026-06-24 22:08:18 -0500

    Creando plantilla de metodos basicos en clase ListaDoblementeEnlazadas

--- src/main/java/org/estudio/ListaDoblementeEnlazadas.java
+++ src/main/java/org/estudio/ListaDoblementeEnlazadas.java
@@ -3,4 +3,24 @@ package org.estudio;
 public class ListaDoblementeEnlazadas {
     Nodo cabeza;
     int tamanio;
+
+
+    public void insertarAlPrincipio() {
+    }
+
+    public void insertarAlFinal() {
+    }
+
+    public void imprimirLista() {
+    }
+
+    public boolean insertarPorIndice() {
+        return false;
+    }
+s
+    public void limpiar() {
+    }
+
+    public void eliminarPorIndice() {
+    }
 }
