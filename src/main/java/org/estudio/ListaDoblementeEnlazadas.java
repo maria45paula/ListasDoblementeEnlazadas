3824c84408d4f99ad47ff70409d1369ffaec6c51
maria45paula <paulaugggvbk@gmail.com>
2026-06-25 23:28:26 -0500

    Eliminar por indice

--- src/main/java/org/estudio/ListaDoblementeEnlazadas.java
+++ src/main/java/org/estudio/ListaDoblementeEnlazadas.java
@@ -15,6 +15,7 @@ public class ListaDoblementeEnlazadas {
     }
 
     public void eliminarAlPrincipio() {
+        Nodo puntero = cabeza;
 
     }
 
@@ -22,7 +23,46 @@ public class ListaDoblementeEnlazadas {
 
     }
 
-    public void eliminarPorIndice() {
+    public int eliminarPorIndice(int indice) {
+        if (indice < 0 || indice >= tamanio) {
+            System.out.println("Error:indice fuera de rango.");
+            return -10000;
+        }
+
+        if (indice == 0) {
+            int valorEliminado = cabeza.valor;
+            cabeza = cabeza.siguiente;
+            if (cabeza != null) {
+                cabeza.anterior = null;
+            }
+            tamanio--;
+            return valorEliminado;
+        }
+
+
+        Nodo puntero = cabeza;
+        int contador = 0;
+
+        while (contador <= indice - 1) {
+            puntero = puntero.siguiente;
+            contador++;
+        }
+
+        if (indice == tamanio - 1) {
+            int valorAEliminar = puntero.valor;
+            puntero.siguiente.anterior = puntero.anterior;
+            puntero.anterior.siguiente = null;
+            tamanio--;
+            return valorAEliminar;
+
+        }
+
+        int valorAEliminar = puntero.valor;
+
+        puntero.siguiente.anterior = puntero.anterior;
+        puntero.anterior.siguiente = puntero.siguiente;
+        tamanio--;
+        return valorAEliminar;
     }
 
     public void limpiar() {
--- src/main/java/org/estudio/Nodo.java
+++ src/main/java/org/estudio/Nodo.java
@@ -3,6 +3,7 @@ package org.estudio;
 public class Nodo {
     int valor;
     Nodo siguiente;
+    Nodo anterior;
 
     public Nodo(int valor) {
         this.valor = valor;
